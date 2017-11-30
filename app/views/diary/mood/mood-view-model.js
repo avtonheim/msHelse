15631ff9a5cdd57f7814110786d68929992a087c
Aleksander Nygård Tonheim <aleksandernygardtonheim@Aleksanders-MacBook-Pro-2.local>
2017-11-30 16:03:26 +0100

    Today I fixed the issue with displaying all entries in the mood graph + implemented how the symptoms can be presented for the user. Now the challenge is how to store data with symptoms (day or night). Also the work on how to generate data to consultation with help of applied data mining.

--- app/views/diary/mood/mood-view-model.js
+++ app/views/diary/mood/mood-view-model.js
@@ -16,45 +16,26 @@ function createViewModel(database) {
       var moodObj = args.object;
       moodObj.style.color = "black";
         new Sqlite("my.db", function(err, db) {
-            db.execSQL("INSERT INTO mood (moodState, timestamp) VALUES (?, datetime())", [moodVal], function(err, id) {
+            db.execSQL("INSERT INTO mood (moodState, timestamp) VALUES (?, ?, date())", [moodVal], function(err, id) {
                 console.log("The new record id is: " + moodText + moodVal);
             });
         });
     }
-    //SELECT moodState, count(moodState) FROM mood group by moodState
-    viewModel.selectdays = function() {
-      this.Mood = new ObservableArray([]);
-          database.all("SELECT moodState, strftime('%d/%m') FROM mood group by timestamp").then(rows => {
-              for(var row in rows) {
-               this.Mood.push({dagsform: rows[row][0], dato: rows[row][1]});
-               console.log(rows[row]);
-             }
-          }, error => {
-              console.log("SELECT ERROR", error);
-          });
-      }
 
       viewModel.selectall = function() {
         this.Mood = new ObservableArray([]);
             database.all("SELECT moodState, timestamp FROM mood group by timestamp").then(rows => {
                 for(var row in rows) {
-                 //this.Mood.push({dagsform: rows[row][0], dato: rows[row][1]});
+                 this.Mood.push({dagsform: rows[row][0], dato: rows[row][1]});
                  console.log(rows[row]);
                }
             }, error => {
                 console.log("SELECT ERROR", error);
             });
         }
-/*
-      // delete a table
-viewModel.deleteTable = function() {
-        database.all("DROP TABLE IF EXISTS mood", [], function(err) {
-            console.log("TABLE DROPPED");
-        });
 
-}
-*/
-  viewModel.selectdays();
+
+  viewModel.selectall();
   return viewModel;
 }
 exports.createViewModel = createViewModel;
