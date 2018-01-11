e3acab6517c8e5c16f6272beb48b2bb8d640871e
Aleksander Nygård Tonheim <aleksandernygardtonheim@Aleksanders-MacBook-Pro-3.local>
2018-01-11 11:26:42 +0100

    Final fix on the breathing exercise before data collection!

--- app/views/training/mindfullness/mindfullness.js
+++ app/views/training/mindfullness/mindfullness.js
@@ -1,19 +1,45 @@
-var animate = require("ui/animation");
+var animationModule = require("ui/animation");
 var frameModule = require("ui/frame")
+var Observable = require("data/observable").Observable;
+
 
 function pageLoaded(args) {
+    var page = args.object;
+
+    var context = new Observable({ messageToUser: "Klikk på blomsten for å starte!" })
+    page.bindingContext = context;
+    //Change label's text
+    context.set("messageToUser", "Fokuser på pusten din og klikk på blomsten for å starte.");
 
 } exports.pageLoaded = pageLoaded;
 
 function startMind(args) {
   var page = args.object;
   var view = page.getViewById("view");
-  view.animate({
-    rotate: 360,
-    duration: 8000
+
+view.animate({ opacity: 2, delay: 3000 })
+    .then(function () { console.log("Pust inn"); })
+    .then(function () { return view.animate({ scale: { x: 2, y: 2 }, duration: 5000, rotate: 360 }); })
+    .then(function () { console.log("Pust ut"); })
+    .then(function () { return view.animate({ scale: { x: 1, y: 1 }, duration: 5000, rotate: 0 }); })
+    .then(function () { return view.animate({ scale: { x: 2, y: 2 }, duration: 5000, rotate: 360 }); })
+    .then(function () { return view.animate({ scale: { x: 1, y: 1 }, duration: 5000, rotate: 0 }); })
+    .then(function () { return view.animate({ scale: { x: 2, y: 2 }, duration: 5000, rotate: 360 }); })
+    .then(function () { return view.animate({ scale: { x: 1, y: 1 }, duration: 5000, rotate: 0 }); })
+    .then(function () { return view.animate({ scale: { x: 2, y: 2 }, duration: 5000, rotate: 360 }); })
+    .then(function () { return view.animate({ scale: { x: 1, y: 1 }, duration: 5000, rotate: 0 }); })
+    .then(function () { return view.animate({ scale: { x: 2, y: 2 }, duration: 5000, rotate: 360 }); })
+    .then(function () { return view.animate({ scale: { x: 1, y: 1 }, duration: 6000, rotate: 180 }); })
+    .then(function () {
+    console.log("Animation finished");
+
+})
+    .catch(function (e) {
+    console.log(e.message);
 });
+
 } exports.startMind = startMind;
 
-function stopMind(){
+function stopMind(args){
   frameModule.topmost().navigate("views/training/training");
 } exports.stopMind = stopMind;
